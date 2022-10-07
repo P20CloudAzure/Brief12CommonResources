0ead73f292e98ec6eeda1022318aff8bf677414b
Ezekiel Matomi Lucky <50213769+ezekiel444@users.noreply.github.com>
2022-10-07 10:51:18 +0200

    sendgrid added but not testing.

--- main/ApiSendgrid/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "api_keyADMIN" {
-  type        = string
-  description = "Api keys admin a jouter pour l'auth a sendgrid"
-}
-
-variable "nameapi" {
-  type =  string
-  description = "nom de l'api qui va être crée"
-}
--- main/variables.tf
+++ main/variables.tf
@@ -109,3 +109,8 @@ variable "api_keyADMIN" {
   type        = string
   description = "Api keys admin a jouter pour l'auth a sendgrid"
 }
+
+variable "nameapi" {
+  type =  string
+  description = "nom de l'api qui va être crée"
+}
